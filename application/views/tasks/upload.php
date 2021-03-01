063d9d51ddb608360d671dab415c8a5028f7cd41
AleksandrTkach <tkach.aleksandr.s@gmail.com>
2021-03-01 15:00:07 +0200

    Fix submit form without file

--- application/views/tasks/upload.php
+++ application/views/tasks/upload.php
@@ -1,6 +1,15 @@
 <div class="coll">
-    <form action="/parser" method="post"  enctype="multipart/form-data">
-        <input name="file" type="file" accept="text/plain">
-        <input type="submit" name="submit" value="Upload">
+    <form action="/parser" method="post"  enctype="multipart/form-data" onsubmit="issetFile(event)">
+        <input id="file" name="file" type="file" accept="text/plain">
+        <input type="submit" value="Upload">
     </form>
 </div>
+
+<script>
+    const file = document.getElementById("file");
+
+    function issetFile(event) {
+        if(file.files.length === 0 )
+            event.preventDefault();
+    }
+</script>
